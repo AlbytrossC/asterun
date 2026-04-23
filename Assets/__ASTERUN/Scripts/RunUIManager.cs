cbdca88c398eaba364479846131e21e9ffead63d
Albytrosss <AlbatrossGames@pm.me>
2026-04-23 14:16:55 +1000

    Shield fixes/changes/tweaks

--- Assets/__ASTERUN/Scripts/RunUIManager.cs
+++ Assets/__ASTERUN/Scripts/RunUIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,12 +13,27 @@ public class RunUIManager : MonoBehaviour
     public float arrowDisplayTime;
     public bool timing;
 
-    [Header("HUD")] 
-    public Slider shieldSlider;
+    [Header("HUD")]
+    [InspectorButton("OnAddShieldClicked")] public bool AddMax;
+    [InspectorButton("OnRemoveShieldClicked")] public bool RemoveMax;
+    [InspectorButton("OnChargeShieldClicked")] public bool AddCharge;
+    [InspectorButton("OnTakeDamageClicked")] public bool TakeDamage;
+    private void OnAddShieldClicked() => AddMaxShieldCharge();
+    private void OnRemoveShieldClicked() => RemoveMaxShieldCharge();
+    private void OnChargeShieldClicked() => ChargeShields();
+    private void OnTakeDamageClicked() => TakeDamageTemp();
 
-    public float chargesHeld;
-    public float chargeDrainSpeed;
-    public float chargeFillSpeed;
+    //public Slider shieldSlider;
+
+    //public float chargesHeld;
+    public int maxShieldCharges;
+    public int currentShieldCharge;
+    public bool shieldIsActive;
+    public GameObject shieldChargePrefab;
+    public Transform shieldChargeParent;
+    public List<GameObject> shieldPrefabs = new List<GameObject>();
+    //public float chargeDrainSpeed;
+    //public float chargeFillSpeed;
 
     private float localTimer = 0;
     private PlayerStatManager _psm;
@@ -26,7 +42,9 @@ public class RunUIManager : MonoBehaviour
     {
         _psm = FindFirstObjectByType<PlayerStatManager>();
         localTimer = 0;
-        chargesHeld = shieldSlider.maxValue;
+        maxShieldCharges = shieldPrefabs.Count;
+        currentShieldCharge = 0;
+        shieldIsActive = true;
     }
     public void Update()
     {
@@ -39,14 +57,14 @@ public class RunUIManager : MonoBehaviour
         if (localTimer > arrowDisplayTime)
             HideArrow();
         
-        UpdateShield();
+        //UpdateShield();
     }
 
-    public void UpdateShield()
+    /*public void UpdateShield() // OLD smooth shield slider
     {
         shieldSlider.value = Mathf.SmoothStep(shieldSlider.value, chargesHeld, chargeDrainSpeed * Time.deltaTime);
         chargesHeld = Mathf.SmoothStep(chargesHeld, shieldSlider.maxValue, chargeFillSpeed * Time.deltaTime);
-    }
+    }*/
     public void ShowArrow()
     {
         arrow.SetActive(true);
@@ -61,14 +79,47 @@ public class RunUIManager : MonoBehaviour
 
     public void TakeDamageTemp()
     {
-        chargesHeld--;
-        if (chargesHeld < 0)
-        {
-            FindFirstObjectByType<PlayerScript>().onDeath.Invoke();
-        }
+        if (!shieldIsActive)
+            Debug.Log("Kill Player! "+currentShieldCharge+"/"+shieldPrefabs.Count+" shield!"); //FindFirstObjectByType<PlayerScript>().onDeath.Invoke();
         else
+            ReduceShields();
+
+        if (currentShieldCharge < shieldPrefabs.Count)
+            shieldIsActive=false;
+    }
+    private void SetShields()
+    {
+        for (int i = 0; i < shieldPrefabs.Count-1; i++)
         {
-            
+            if (i <= currentShieldCharge)
+                shieldPrefabs[i].GetComponent<shieldPrefab>().SetFull();
+            else shieldPrefabs[i].GetComponent<shieldPrefab>().SetEmpty();
         }
+            
+
+    }
+
+    private void ChargeShields()
+    {
+        currentShieldCharge++;
+        SetShields();
+    }
+    private void ReduceShields()
+    {
+        currentShieldCharge--;
+        SetShields();
+    }
+
+    public void AddMaxShieldCharge()
+    {
+
+        shieldPrefabs.Add(Instantiate(shieldChargePrefab, shieldChargeParent));
+        maxShieldCharges++;
+    }
+    public void RemoveMaxShieldCharge()
+    {
+        Destroy(shieldPrefabs[shieldPrefabs.Count-1]);
+        shieldPrefabs.RemoveAt(shieldPrefabs.Count-1);
+        maxShieldCharges--;
     }
 }
---
+++ Assets/__ASTERUN/Scripts/shield/shieldPrefab.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class shieldPrefab : MonoBehaviour
+{
+    public GameObject activeIcon;
+    public bool isFull;
+    private void OnEnable()
+    {
+        isFull = false;
+        ToggleActiveIcon();
+    }
+
+    public void ToggleActiveIcon()
+    {
+        switch (isFull)
+        {
+            case true:
+                activeIcon.SetActive(true);
+                break;
+            case false:
+                activeIcon.SetActive(false);
+                break;
+        }
+    }
+
+    public void SetFull() => activeIcon.SetActive(true);
+    public void SetEmpty() => activeIcon.SetActive(false);
+    public void Activate() => gameObject.SetActive(true);
+    public void DeActivate() => gameObject.SetActive(false);
+
+}
